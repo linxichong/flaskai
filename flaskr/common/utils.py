f6be0b10704c954f2c4f0ff846e9d060b07cc9a9
linxichong <gogailinxichong@gmail.com>
2019-06-09 11:38:34 +0900

    modify
    Signed-off-by: linxichong <gogailinxichong@gmail.com>

--- flaskr/common/utils.py
+++ flaskr/common/utils.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 import os
 
+
 def get_spider_db():
     db_url = os.environ.get('MONGODB_URI')
     # db_url = 'mongodb://heroku_qmxbd0rq:ef13vosvbgegmocr09svn98h5@ds263816.mlab.com:63816/heroku_qmxbd0rq'
